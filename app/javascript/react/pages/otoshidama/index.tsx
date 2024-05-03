8ad8874b4a2796f9dddf79007ab0ee7879ffc5e2
g.sawada <sawada.gota@gmail.com>
2024-05-03 22:56:15 +0900

    rechartsのインストールと仮グラフの作成

--- app/javascript/react/pages/otoshidama/index.tsx
+++ app/javascript/react/pages/otoshidama/index.tsx
@@ -1,10 +1,62 @@
 import React from 'react';
+import { 
+  BarChart,
+  Bar,
+  Brush,
+  ReferenceLine,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer } from 'recharts';
+
+  const data = [
+    { name: '1', uv: 300, pv: 456 },
+    { name: '2', uv: -145, pv: 230 },
+    { name: '3', uv: -100, pv: 345 },
+    { name: '4', uv: -8, pv: 450 },
+    { name: '5', uv: 100, pv: 321 },
+    { name: '6', uv: 9, pv: 235 },
+    { name: '7', uv: 53, pv: 267 },
+    { name: '8', uv: 252, pv: -378 },
+    { name: '9', uv: 79, pv: -210 },
+    { name: '10', uv: 294, pv: -23 },
+    { name: '12', uv: 43, pv: 45 },
+    { name: '13', uv: -74, pv: 90 },
+    { name: '14', uv: -71, pv: 130 },
+    { name: '15', uv: -117, pv: 11 },
+    { name: '16', uv: -186, pv: 107 },
+    { name: '17', uv: -16, pv: 926 },
+    { name: '18', uv: -125, pv: 653 },
+    { name: '19', uv: 222, pv: 366 },
+    { name: '20', uv: 372, pv: 486 },
+  ];
 
 export default function Otoshidama() {
   return (
-    <div className="text-2xl m-10">
-      <h1>Otoshidama App</h1>
-      <p>It works!</p>
-    </div>
+    <ResponsiveContainer height={600} minWidth={400}>
+      <BarChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend verticalAlign="top" wrapperStyle={{ lineHeight: '40px' }} />
+      <ReferenceLine y={0} stroke="#000" />
+      <Brush dataKey="name" height={30} stroke="#8884d8" />
+      <Bar dataKey="pv" fill="#8884d8" />
+      <Bar dataKey="uv" fill="#82ca9d" />
+    </BarChart>
+  </ResponsiveContainer>
   );
 }
