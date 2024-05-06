3be952210a4e9b0e323302656cd64b40a4e339d6
g.sawada <sawada.gota@gmail.com>
2024-05-06 17:53:23 +0900

    assets.rbのプリコンパイルファイルを設定

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,11 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+#ここを追加。書き方は色々ある模様
+Rails.application.config.assets.precompile += %w(
+  react/entrypoints/fifty_meter_entry.tsx
+  react/entrypoints/future_entry.tsx
+  react/entrypoints/otoshidama_entry.tsx
+  react/entrypoints/subject_entry.tsx
+)
