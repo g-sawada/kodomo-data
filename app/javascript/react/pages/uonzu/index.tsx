fc88d9cf986097e7d2cb76ce75e9b08a42610d66
g.sawada <sawada.gota@gmail.com>
2024-05-08 12:19:48 +0900

    軸の調整

--- app/javascript/react/pages/uonzu/index.tsx
+++ app/javascript/react/pages/uonzu/index.tsx
@@ -26,41 +26,46 @@ const customizedAxisTick = (props) => {
 }
 
 export default function Uonzu() {
+  const style = {fontFamily: "sans-serif, serif"}; //sans-serif→ゴシック，serif→明朝
   return (
-    <ResponsiveContainer height={500} width={500}>
-      <ComposedChart
-        width={500}
-        height={400}
-        data={data_tokyo}
-        margin={{
-          top: 20,
-          right: 20,
-          bottom: 20,
-          left: 20,
-        }}
-      >
-        <CartesianGrid stroke="#f5f5f5" />
-        <XAxis 
-          dataKey="month"
-          // scale="band"   //こうすると，目盛りの位置が左にずれる
-          scale="auto"
-          />
-        <YAxis 
-          yAxisId={1}
-          label={{value: "降水量", angle: -90, dx: -20}}
-          domain={[0, 700]}
-          tickCount={8}
-          />
-        <YAxis
-          yAxisId={2}
-          orientation="right"
-          label={{value: "気　温", angle: 90, dx: 20}}
-          domain={[-30, 40]}
-          tickCount={8}/>
-        <Bar yAxisId={1} dataKey="rain" barSize={20} fill="#413ea0" />
-        <Line yAxisId={2} type="monotone" dataKey="temp_ave" stroke="red" />
-        <Tooltip />
-      </ComposedChart>
-    </ResponsiveContainer>
+    <div style={style}>
+      <ResponsiveContainer height={500} width={500}>
+        <ComposedChart
+          width={500}
+          height={400}
+          data={data_tokyo}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <CartesianGrid 
+            strokeDasharray="10 30"
+            vertical={false}
+            />
+          <XAxis 
+            dataKey="month"
+            scale="auto"
+            />
+          <YAxis 
+            yAxisId={1}
+            label={{value: "降水量", angle: -90, dx: -20}}
+            domain={[0, 700]}
+            tickCount={8}
+            />
+          <YAxis
+            yAxisId={2}
+            orientation="right"
+            label={{value: "気　温", angle: 90, dx: 20}}
+            domain={[-30, 40]}
+            tickCount={8}/>
+          <Bar yAxisId={1} dataKey="rain" barSize={20} fill="#413ea0" />
+          <Line yAxisId={2} type="monotone" dataKey="temp_ave" stroke="red" />
+          <Tooltip />
+        </ComposedChart>
+      </ResponsiveContainer>
+    </div>
   );
 }
